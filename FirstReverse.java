2fea6fa5780c46dbe3b6b8c7cd99973ebfff38a6
dcsheive <dcsheive@gmail.com>
2018-11-07 15:24:28 -0500

    first commit

---
+++ FirstReverse.java
@@ -0,0 +1,19 @@
+import java.util.*; 
+import java.io.*;
+
+class FirstReverse {  
+  public static String FirstReverseMethod(String str) { 
+  
+    String newStr = ""; 
+    for (int i = str.length()-1; i>=0;i--){
+        newStr = newStr + str.charAt(i);
+    }
+    return newStr;
+    
+  } 
+  
+  public static void main (String[] args) {  
+    System.out.print(FirstReverseMethod("Hello")); 
+  }   
+  
+}
